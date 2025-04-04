d9fdae1c3190434afdd500e97381eea39c854220
MinecraftFan69420 <gelay232000@yahoo.com>
2025-04-04 22:43:10 +0300

    Forgot to make the thing unlock on right password

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ function devauthenticate() {
     const correct_password = "*#&*[Qgh5]7$y"
     if (!player.consoleunlocked) {
         password_input = prompt("Enter Dev Console password:")
-        if (password_input === correct_password) { alert("Correct!"); swaptab(6) }
+        if (password_input === correct_password) { alert("Correct!"); swaptab(6); player.consoleunlocked = true }
         else alert("INCORRECT! Only a developer would know the password!")
     } else swaptab(6)
 }
