6b99698fc2757d3848dd19a77b82c3f64da72e67
MinecraftFan69420 <gelay232000@yahoo.com>
2025-04-12 09:40:52 +0300

    Gotta comply with standard

--- Tetrational pain.js
+++ Tetrational pain.js
@@ -1,19 +1,15 @@
-function log(num)
-  {return (Math.log(num)/2.302585092994046)}
+function log(num) {return (Math.log(num)/2.302585092994046)}
 
-function round(num, places)
-  {return ((Math.round((num)/(10 ** places)))*(10 ** places))}
+function round(num, places) {return ((Math.round((num)/(10 ** places)))*(10 ** places))}
 
-function SLog(num)
-{
+function SLog(num) {
   if (num < 1) {return (num - 1)}
   else if (num < 10) {return log(num)}
   else if (num < 1e10) {return (log(log(num))+1)}
   else if (num >= 1e10) {return (log(log(log(num)))+2)}
 }
 
-function UndoSLog(num)
-{
+function UndoSLog(num){
   if (num < 0) {return (round((num + 1),-4))}
   else if (num < 1) {return (round((10 ** num)),-3)}
   else if (num < (1+log(3))) {return (round((10 ** (10 ** (num % 1))),-2))}
