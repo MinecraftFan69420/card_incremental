ba0ae53107741d90add5db468f5803d1ad8aa22b
RandomGamerPerson111 <noperson.png@gmail.com>
2025-04-12 16:10:02 -0700

    Added adition

--- Tetrational pain.js
+++ Tetrational pain.js
@@ -43,3 +43,69 @@ function UndoSLog(num){
   else if (num < 1e10) {base = (floor((10 ** (num % 1)), -3)).toFixed(0); return ("E" + base + "#" + floor(num, 0))}
   else return ("E#"+floor(num,0))
 }
+
+function addSLogs(num1, num2)
+{
+  if (num2 > num1) {addSLog(num2, num1)}
+  else 
+  {class1 = Math.max(floor(num1,0),-1)
+  class2 = Math.max(floor(num2,0),-1)
+  PowDif = (num1-num2)
+  Switch (class2) 
+    {case -1:
+      Switch (class1) {
+        case -1:
+          output = (num1+num2+1)
+          if (output > 0) {return log(output+1)} else {return output}
+          break;
+        case 0:
+          output = log((10**num1)+(num2+1))
+          if (output > 1) {return (1+log(output))} else {return output}
+          break;
+        case 1:
+          output = 1+log(log((10**(10**(num1-1)))+(num2+1)))
+          if (output > 2) (return 2+log(output-1)) else {return output}
+          break;
+        default:
+          return num1
+          break;}
+      break;
+     case 0:
+       Switch (class1) {
+         case 0:
+           output = log((10**num1)+(10**num2))
+           if (output > 1) {return (1+log(output))} else {return output}
+           break;
+         case 1:
+           output = 1+log(log((10**(10**(num1-1)))+(10**num2)))
+           if (output > 2) {return 2+log(output-1)} else {return output}
+           break;
+         default:
+           return num1
+           break;}
+        break;
+     case 1:
+       Switch (class1) {
+         case 1:
+           output = 1+log(log((10**(10**(num1-1)))+(10**(10**(num2-1)))))
+           if (output > 2) {return 2+log(output-1)} else {return output}
+           break;
+         case 2:
+           output = 2+log(log((10**(10**(num1-2)))+(log(1+(10**((10**(num2-1))-(10**(10**(num1-2)))))))))
+           if (output > 3) {return 3+log(output-2)} else {return output}
+           break;
+         default:
+           return num1
+           break;}
+       break;
+     case 2:
+       if (class1 = 2) {
+         output = 2+log(log((10**(10**(num1-2)))+(log(1+(10**((10**(10**(num2-2)))-(10**(10**(num1-2)))))))))
+         if (output > 3) {return 3+log(output-2)} else {return output}}
+       else {return num1}
+       break;
+     default:
+       return num1
+       break;}
+  }
+}
