8c6deaca0626cb0779411b53f424fea41a0bb5ba
MinecraftFan69420 <gelay232000@yahoo.com>
2025-10-25 10:28:27 +0300

    changed card costs to new Decimals

--- JS/cards.js
+++ JS/cards.js
@@ -1,20 +1,20 @@
 const cards = {
     regular: {
-        1: {cost: 20},
-        2: {cost: 200},
-        3: {cost: 500},
-        4: {cost: 2000},
-        5: {cost: 2000},
-        6.1: {cost: 5000}, 6.2: {cost: 5000},
-        7: {cost: 10_000},
-        8: {cost: 31_415},
-        9.1: {cost: 100_000}, 9.2: {cost: 100_000},
-        10: {cost: 500_000},
-        11: {cost: 694_200},
-        12: {cost: 1_500_000,}
+        1: {cost: new Decimal(20)},
+        2: {cost: new Decimal(200)},
+        3: {cost: new Decimal(500)},
+        4: {cost: new Decimal(2000)},
+        5: {cost: new Decimal(2000)},
+        6.1: {cost: new Decimal(5000)}, 6.2: {cost: new Decimal(5000)},
+        7: {cost: new Decimal(10_000)},
+        8: {cost: new Decimal(31_415)},
+        9.1: {cost: new Decimal(100_000)}, 9.2: {cost: new Decimal(100_000)},
+        10: {cost: new Decimal(500_000)},
+        11: {cost: new Decimal(694_200)},
+        12: {cost: new Decimal(1_500_000)}
     },
     charge: {
-        1: {cost: [500_000, 200]}, // about cost: first element is points, second one is charge
-        2: {cost: [1_000_000, 500]}
+        1: {cost: [new Decimal(500_000), new Decimal(200)]}, // about cost: first element is points, second one is charge
+        2: {cost: [new Decimal(1_000_000), new Decimal(500)]}
     }
 }
