2b1e26b02939cf7265b2df492895c038cc5adbfc
MinecraftFan69420 <gelay232000@yahoo.com>
2025-10-25 10:36:05 +0300

    Refactor player object to use Decimal for values

--- JS/player.js
+++ JS/player.js
@@ -1,14 +1,14 @@
 const default_player = {
-    points: 0,
+    points: new Decimal(0),
     ppc: {
-        base: 1,
+        base: new Decimal(1),
         mult: { // A list of multipliers to the point gain.
-            C5: 1,
-            C6A: 1, C6B: 1,
-            C9A: 1, C9B: 1,
-            constants: 1, // The constant multipliers
-            totalmanual: 1, // Total of manual bonuses
-            totalauto: 1, // Total of autoclicker multipliers
+            C5: new Decimal(1),
+            C6A: new Decimal(1), C6B: new Decimal(1),
+            C9A: new Decimal(1), C9B: new Decimal(1),
+            constants: new Decimal(1), // The constant multipliers
+            totalmanual: new Decimal(1), // Total of manual bonuses
+            totalauto: new Decimal(1), // Total of autoclicker multipliers
         }
     },
     card_possession: { // Costs of cards & if the player has them.
@@ -31,13 +31,13 @@ const default_player = {
         }
     },
     buyables: { // Cost of buyables & how many the player has, and maximum purchases. e308 means no limit.
-        1: { amount: 0, cost: 20, maxpurchases: 1e308 },
-        2: { amount: 0, cost: 100, maxpurchases: 5 },
-        3: { amount: 0, cost: 1000, maxpurchases: 1e308 }
+        1: { amount: new Decimal(0), cost: new Decimal(20), maxpurchases: Infinity},
+        2: { amount: new Decimal(0), cost: new Decimal(100), maxpurchases: 5},
+        3: { amount: new Decimal(0), cost: new Decimal(1000), maxpurchases: Infinity }
     },
-    autoclicker: { strength: 0, cooldown:  Infinity, cps: 0 }, // Stats, cooldown in ticks. 
+    autoclicker: { strength: 0, cooldown: new Decimal(1, Infinity, Infinity), cps: 0 }, // Stats, cooldown in ticks. 
     defaultcooldowns: { 0: Infinity, 1: 20, 2: 10, 3: 5, 4: 2, 5: 1, 6: 1, 7: 1, current: Infinity }, // Default autoclicker cooldowns in ticks
-    charge: {amount: 0, req: 1_000_000, unlocked: false, times: 0, persecond: 0}, // Charge resource, requirement to prestige, and how many times the player has prestiged
+    charge: {amount: new Decimal(0), req: new Decimal(1_000_000), unlocked: false, times: new Decimal(0), persecond: new Decimal(0)}, // Charge resource, requirement to prestige, and how many times the player has prestiged
     consoleunlocked: false
 }
 
